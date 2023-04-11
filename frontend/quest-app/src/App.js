06a90c3194ec117daca12f5c642a6da0d90db5a9
Coders club <1yavuzahmet@gmail.com>
2023-04-11 23:38:43 +0300

    question-answer-application frontend Home and User Navbar created

--- frontend/quest-app/src/App.js
+++ frontend/quest-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import User from './components/User/User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Hello word</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Route>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact route="/users/:userId" component={User}> </Route>
+      </Route>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ frontend/quest-app/src/components/User/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User() {
+
+const {userId}=useParams();
+return(
+    <div>
+        User !! {userId}
+    </div>
+)
+    
+}
+export default User;
